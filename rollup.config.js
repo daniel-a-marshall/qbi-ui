f755c768389a888ec02fe8d52b534518b1b8a04a
Dan Marshall <daniel_a_marshall@whirlpool.com>
2023-04-25 06:41:52 -0400

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const dts = require("rollup-plugin-dts");
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "cjs" }],
+    plugins: [dts.default()],
+  },
+];
