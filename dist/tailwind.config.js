bc4739bdb9aed80a2009e93e47e43e1a0ec524ef
Dan Marshall <daniel_a_marshall@whirlpool.com>
2023-05-10 14:49:02 -0400

    update build

--- dist/tailwind.config.js
+++ dist/tailwind.config.js
@@ -14,26 +14,8 @@ module.exports = {
           500: "#0092cc",
           600: "#006e99",
           700: "#0D436B",
-        },
-        gray: {
-          50: "#F3F5F7",
-          100: "#E7EAEE",
-          150: "#CFD5DE",
-          200: "#ABB6C4",
-          300: "#8797AB",
-          400: "#6F829C",
-          500: "#546478",
-          600: "#3B4654",
-          650: "#2A323C",
-          700: "#20262E",
-          800: "#191E24",
-          900: "#080A0C",
-        },
-      },
-      fontFamily: {
-        heading: ["Nunito Sans", "sans-serif"],
-        body: ["Open Sans", "sans-serif"],
-      },
+        }
+      }
     },
   },
   plugins: [
